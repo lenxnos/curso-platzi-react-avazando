e184968b09129b28c573b91fd5c479fd51e17828
Nelson Soto <nelson.soto.c05@gmail.com>
2021-10-28 23:54:21 -0400

    feat: Add fetch to category with useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => (
-  <List>
-    {
-      categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-    }
-  </List>
-)
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(function () {
+    window.fetch('https://petgram-server-lenxnos.vercel.app/categories')
+      .then(response => response.json())
+      .then(response => setCategories(response))
+  }, [])
+  return (
+    <List>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+}
