9545a8e090b7bbe6dff70bab66c0e25b31e23839
Nelson Soto <nelson.soto.c05@gmail.com>
2021-10-29 00:08:59 -0400

    feat: Add float list of categories options

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,16 +4,35 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(function () {
     window.fetch('https://petgram-server-lenxnos.vercel.app/categories')
       .then(response => response.json())
       .then(response => setCategories(response))
   }, [])
-  return (
-    <List>
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
