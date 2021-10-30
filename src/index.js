e724f8e27b1b384ad9538a042514c8dc82f75e22
Nelson Soto <nelson.soto.c05@gmail.com>
2021-10-30 10:55:50 -0400

    feat: Add React Apollo and configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-lenxnos.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
