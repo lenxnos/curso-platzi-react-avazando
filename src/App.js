57604a01f980f7ad47bccc16e7fb24f9027d0e86
Nelson Soto <nelson.soto.c05@gmail.com>
2021-10-30 15:47:46 -0400

    feat: Add page details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
+import { Detail } from './pages/Details'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:id' />
-            </Router>
-            )
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
       <GlobalStyles />
     </div>
   )
