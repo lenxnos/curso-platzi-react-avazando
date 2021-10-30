5d83ba5b80896475d26d7acf33b47bc1a271077d
Nelson Soto <nelson.soto.c05@gmail.com>
2021-10-30 15:26:31 -0400

    chore: Add @react/router package

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   output: {
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin(
@@ -25,5 +26,10 @@ module.exports = {
         }
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: {
+      disableDotRule: true
+    }
   }
 }
